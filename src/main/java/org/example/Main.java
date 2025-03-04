b5258f76aa054bf15c56e57f08d5be53174ce529
kqjm1 <kqk9269@gmail.com>
2025-03-04 18:29:39 +0900

    ww

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+//            int i = 0;
+//            for(i=0;i<100;i++) {
+//                if(i%2==0){
+//                    System.out.println(i);
+//                }
+
+            }
+    }
+}

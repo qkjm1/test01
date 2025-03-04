5509be6f4f859510c54182210cdd3c03d9af3cad
qkjm1 <kqk9269@gmail.com>
2025-03-04 16:51:23 +0900

    테스트66

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,80 +1,90 @@
 package org.example;
 
-// 문제 : 할인 대상인지 아닌지 출력해주세요.
-// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
-// 조건 : 출력예시 처럼 출력되어야 합니다.
-// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
-// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
-// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
 
-import java.util.EventListener;
+
+// 문제 : 아래와 같이 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int age = 60; // 이 값을 바꿔가면서 실행해보세요.
 
-        System.out.println("당신의 나이는 " + age + "살 입니다.");
+        // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
 
-//        // 구현시작
+//        System.out.println("8"+"*"+"1"+"="+8*1);
+//        System.out.println("8"+"*"+"2"+"="+8*2);
+//        System.out.println("8"+"*"+"3"+"="+8*3);
+//        System.out.println("8"+"*"+"4"+"="+8*4);
+//        System.out.println("8"+"*"+"5"+"="+8*5);
+//        System.out.println("8"+"*"+"6"+"="+8*6);
+//        System.out.println("8"+"*"+"7"+"="+8*7);
+//        System.out.println("8"+"*"+"8"+"="+8*8);
+//        System.out.println("8"+"*"+"9"+"="+8*9);
 
-
-//        if (age <= 19 || age >= 60) {
-//             System.out.println("할인대상");
-//        }
-//        else {
-//            System.out.println("할인대상이 아님");
-//        }
-
-
-
-//        if (age > 19 && age <60){
-//            System.out.println("할인대상이 아닙니다");
-//        }
-//        else {
-//           System.out.println("할인대상");
-//        }
-
-
-
-//        if (age < 20){
-//            System.out.println("할인대상");
-//        }
-//        else if (age >=60 ){
-//            System.out.println("할인대상");
-//        }
-//        else {
-//            System.out.println("할인대상이 아님");
-//        }
-
-
-
-//        if (age >= 60){
-//            System.out.println("할인대상");
-//        }
-//        else if (age < 20){
-//           System.out.println("할인대상");
-//        }
-//        else {
-//          System.out.println("할인대상이 아님");
-//        }
-
-
-//        if (age >= 60){
-//            System.out.println("할인대상");
-//        }
-//        if (age <= 19){
-//            System.out.println("할인대상");
-//        }
+//        System.out.println("8 * 1 = "+8*1);
+//        System.out.println("8 * 2 = "+8*2);
+//        System.out.println("8 * 3 = "+8*3);
+//        System.out.println("8 * 4 = "+8*4);
+//        System.out.println("8 * 5 = "+8*5);
+//        System.out.println("8 * 6 = "+8*6);
+//        System.out.println("8 * 7 = "+8*7);
+//        System.out.println("8 * 8 = "+8*8);
+//        System.out.println("8 * 9 = "+8*9);
 
 
 
+//        int dan = 8;
+//
+//        System.out.println("8 * 1 = "+dan*1);
+//        System.out.println("8 * 2 = "+dan*2);
+//        System.out.println("8 * 3 = "+dan*3);
+//        System.out.println("8 * 4 = "+dan*4);
+//        System.out.println("8 * 5 = "+dan*5);
+//        System.out.println("8 * 6 = "+dan*6);
+//        System.out.println("8 * 7 = "+dan*7);
+//        System.out.println("8 * 8 = "+dan*8);
+//        System.out.println("8 * 9 = "+dan*9);
 
 
-        // 출력 => 할인대상입니다. 또는 할인 대상이 아닙니다.
+//        int dan = 5;
+//
+//        System.out.println(dan+" * 1 = "+dan*1);
+//        System.out.println(dan+" * 2 = "+dan*2);
+//        System.out.println(dan+" * 3 = "+dan*3);
+//        System.out.println(dan+" * 4 = "+dan*4);
+//        System.out.println(dan+" * 5 = "+dan*5);
+//        System.out.println(dan+" * 6 = "+dan*6);
+//        System.out.println(dan+" * 7 = "+dan*7);
+//        System.out.println(dan+" * 8 = "+dan*8);
+//        System.out.println(dan+" * 9 = "+dan*9);
+
+
+//        int dan = 9;
+//
+//        int i = 1;
+//
+//        System.out.println(dan+" * "+i+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+//        System.out.println(dan+" * "+(++i)+" = "+dan*i);
+
+
+
+
+
     }
 }
-
-
-
-
-

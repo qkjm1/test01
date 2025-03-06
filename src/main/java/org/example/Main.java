c58141b9dbece071fcd995bcdfd6d248ab583dfe
seongho00 <tjdwns000927@gmail.com>
2025-03-06 11:05:29 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,21 +8,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char x;
-        char y;
-
-        x = sc.next().charAt(0);
-        y = sc.next().charAt(0);
-
-        System.out.printf("%c %c",y,x);
 
     }
 }
-
-//class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello");
-//
-//    }
-//

2038c2ac66183eb53c44963642c15257883ac322
qkjm1 <kqk9269@gmail.com>
2025-03-07 16:51:31 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,29 +5,73 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr= {5,1,4} ;
-        int[] a = new int [arr[0]+arr[1]+arr[2]];
-        int b=0;
+//        int[] arr= {5,1,4} ;
+//        int[] a = new int [arr[0]+arr[1]+arr[2]];
+//        int b=0;
+//
+////        for (int i=0;i<arr.length;i++){
+//            for (int j=0;j<arr[0];j++){
+//                a[j]=arr[0];
+//                if(a[j]<arr.length){break;}
+//            }
+//            for (int k=arr[0]+arr[1];k<arr[1];k++){
+//                a[k]=arr[1];
+//            }
+//            for (int m=arr[0]+arr[1]+arr[2];m<arr[2];m++){
+//                a[m]=arr[2];
+//            }
+////        }
+//for (int t=0;t<a.length;t++) {
+//    System.out.println(a[t]);
+//}
+
+//        boolean arr1 [] = {true, false, true};
+//        float arr2 [] ={3.14f,7.77f,11,11f};
+//        int arr3 [] = {1,2,3,4,5,6,7,8,9,10};
+//
+
+        boolean arr1 [] = new boolean[3];
+        float arr2 []  = new float[3];
+        int arr3 [] = new int[11];
+        
+//        System.out.println(arr1[0]);
+//        System.out.println(arr1[1]);
+//        System.out.println(arr1[2]);
+//        System.out.println(arr2[0]);
+//        System.out.println(arr2[1]);
+//        System.out.println(arr2[2]);
+//
+//        for (int i = 0;i<arr3.length;i++){
+//            System.out.println(arr3[i]);
+//        }
 
 
-            for (int j=0;j<arr[0];j++){
-                a[j]=arr[0];
-            }
-            for (int k=arr[0]+arr[1];k<arr[1];k++){
-                a[k]=arr[1];
-            }
-            for (int m=arr[0]+arr[1]+arr[2];m<arr[2];m++){
-                a[m]=arr[2];
-            }
 
-for (int t=0;t<a.length;t++) {
-    System.out.println(a[t]);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
     }
 }
+
 
 
 

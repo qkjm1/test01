1191c638b923fe5ca9043020c8beefe6842911ab
qkjm1 <kqk9269@gmail.com>
2025-03-07 16:15:59 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,31 @@
 package org.example;
 
-
-
-
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr= {5,1,4} ;
+        int[] a = new int [arr[0]+arr[1]+arr[2]];
+        int b=0;
 
-        Scanner sc = new Scanner(System.in);
 
-        String str = new String();
-        char c = ' ';
-        str = sc.next();
-        c = str.charAt(0);
+            for (int j=0;j<arr[0];j++){
+                a[j]=arr[0];
+            }
+            for (int k=arr[0]+arr[1];k<arr[1];k++){
+                a[k]=arr[1];
+            }
+            for (int m=arr[0]+arr[1]+arr[2];m<arr[2];m++){
+                a[m]=arr[2];
+            }
 
-        for (int i=0;i<str.length();i++){
-            c = str.charAt(i);
-            System.out.printf("'%c'\n",c);
-        }
+for (int t=0;t<a.length;t++) {
+    System.out.println(a[t]);
+}
+
+
+
     }
 }
 

a1db56052145f8f54b5ce914f87089071169c7ca
kqjm1 <kqk9269@gmail.com>
2025-03-11 00:43:06 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-//        int[] array = new int[100];
-//        // array에 순서대로 1부터 100까지 정수를 넣어보세요.
-//            for (int i=0;i<array.length;i++){
-//                array[i]=i+1;
-//            }
-//
-//        System.out.println(array[56]);
-//        // 아래는 결과 평가를 위한 코드입니다. 수정하지 마세요.
+        d dds = new d();
+        dds.a=10;
+        System.out.println(dds.a);
 
-//        int[] array = new int[10];
-//        for (int i=0;i<array.length;i++){
-//            array[i] = 2 * i;
-//        }
-//
-//        System.out.println(array[1]);
-
-        int sum = 0;
-
-        int[] array = new int[] {1,2,3,4,5};
-
-        for (int i=0;i<array.length;i++){
-            sum = sum + array[i];
+        d dss = new d();
+        dss.a=2;
+        System.out.println(dss.a);
         }
 
-        System.out.println(sum);
+}
 
+class d {
+    int a;
 
-
-    }
 }

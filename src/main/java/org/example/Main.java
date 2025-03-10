8e2bd8c354e70888b8b3946ee0f827985768c555
kqjm1 <kqk9269@gmail.com>
2025-03-11 01:04:00 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,33 +20,53 @@ public class Main {
 //        b플레이어2.직업= "소드마스터";
 
 
-        자동차 a;
-        자동차 b;
-        자동차 c;
+//        자동차 a;
+//        자동차 b;
+//        자동차 c;
+//
+//        자동차 a = new 자동차;
+//        자동차 b = new 자동차;
+//        자동차 c = new 자동차;
+//
+//        a.속력=80;
+//        b.속력=100;
+//        c.속력=60;
+//
+//        System.out.println(a.속력);
+//
+//
+//
+//    }
+//}
+////class 플레이어{
+////    int 나이;
+////    String 이름;
+////    String 직업;
+//}
+//class  자동차{
+//    double 속력;
+//}
+        d dds = new d();
+        dds.a=10;
+        System.out.println(dds.a);
 
-        자동차 a = new 자동차;
-        자동차 b = new 자동차;
-        자동차 c = new 자동차;
 
-        a.속력=80;
-        b.속력=100;
-        c.속력=60;
 
-        System.out.println(a.속력);
+        for (int i=0;i<array.length;i++){
+            sum = sum + array[i];
+            d dss = new d();
+            dss.a=2;
+            System.out.println(dss.a);
+        }
 
 
 
     }
-}
-//class 플레이어{
-//    int 나이;
-//    String 이름;
-//    String 직업;
-}
-class  자동차{
-    double 속력;
-}
 
 
+}
+        class d {
+            int a;
 
+        }
 

c91069f4f4c19c3b34b58e0081891de694407bd5
qkjm1 <kqk9269@gmail.com>
2025-03-11 10:52:26 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,43 +5,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        hu a = new hu();
-//        a.age=34;
-//        a.name="김철수";
-//        a.married= true;
-//        a.intro();
-//        System.out.println(a.age);
-//
-//        hu v = new hu();
-//        v.age=65;
-//        v.name="홍길동";
-//        v.married= false;
-//        v.intro();
-//        System.out.println(v.married);
-//    }
-//}
-//class hu {
-//    int age;
-//    String name;
-//    boolean married;
-//
-//    void intro(){
-////        int age = this.age;
-////        String name = this.name;
-////        boolean married = this.married;
-//        System.out.println("__자기소개__");
-//        System.out.printf("이름: %s\n",name);
-//        System.out.printf("나이: %d\n", age);
-//        System.out.printf("결혼: %b\n",married);
 
-
-
-
+        
+        abc.인사();
 
 
     }
 }
-
+class abc {
+    static void 인사(){
+        System.out.println("안녕");
+    }
+}
 
 
 

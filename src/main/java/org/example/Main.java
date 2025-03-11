2a270c508db34a4c7fac1c42d7d6ebd807d9c114
kqjm1 <kqk9269@gmail.com>
2025-03-12 06:54:19 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import java.awt.*;
-import java.util.Arrays;
-import java.util.Scanner;
+//import java.awt.*;
+//import java.util.Arrays;
+//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,27 +15,38 @@ public class Main {
         result = Math.isPrimeNumber(3);
         System.out.println("3은(는) 소수인가? : " + result);
 
-        result = Math.isPrimeNumber(4);
-        System.out.println("4은(는) 소수인가? : " + result);
+        result = Math.isPrimeNumber(7);
+        System.out.println("7은(는) 소수인가? : " + result);
 
-//        int rs = one_to_n_prime_numbers_count(10);
-//        System.out.println("rs : " + rs);
-//        // rs : 4
-//        rs = one_to_n_prime_numbers_count(13);
-//        System.out.println("rs : " + rs);
-//        // rs :  6
-//    }
-//    public static int one_to_n_prime_numbers_count(){
-//        int g = 0;
-//        for(int i=0;;){
-//            if (){
-//
-//            }
-//        }
+        int rs = one_to_n_prime_numbers_count(10);
+        System.out.println("rs : " + rs);
+        // rs : 4
+        rs = one_to_n_prime_numbers_count(13);
+        System.out.println("rs : " + rs);
+        // rs :  6
 
-        boolean rs = Math.IsevenNumber(3);
-        System.out.println(rs);
+        boolean rs1 = Math.IsevenNumber(8);
+        System.out.println("짝수인가 :"+rs1);
     }
+
+
+    public static int one_to_n_prime_numbers_count(int c){
+        int g = 0;
+        for (int m=1;m<c;m++){
+            for (int i=2;i<=c;i++){
+                if(c%2==1){
+                    g += 1;
+                }
+                if (c==2){
+                    g=1;
+                    break;
+                }
+            }
+        }
+        return g;
+
+    }
+
 }
 
 class Math {
@@ -43,7 +54,13 @@ class Math {
         return  k % 2 == 0;
     }
     public static boolean isPrimeNumber(int a){
-        return a%2==0;
+        for (int i=2;i<=a;i++){
+             if(a%2==1||a==2){
+                return true;
+            }
+        }
+
+        return false;
 
     }
 

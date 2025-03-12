fd0a9b337eac9cf4315d9d6064a40eb8c8175379
qkjm1 <kqk9269@gmail.com>
2025-03-12 16:50:28 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,118 +6,64 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-//        천둥오리 a천둥오리 = new 천둥오리();
-//        a천둥오리.날다();
+//        전사 a전사 = new 전사();
 //
-//        흰오리 a흰오리 = new 흰오리();
-//        a흰오리.날다();
+//        a전사.공격("브라이언", "칼");
+//        // 브라이언이(가) 칼(으)로 공격합니다.
 //
-//        고무오리 a고무오리 = new 고무오리();
-//        a고무오리.날다();
+//        a전사.재공격();
+//        // 브라이언이(가) 칼(으)로 공격합니다.
 //
-//        고무2오리 a고무2오리 = new 고무2오리();
-//        a고무2오리.날다();
+//        a전사.공격("필립", "창");
+//        // 필립이(가) 창(으)로 공격합니다.
 //
+//        a전사.공격("마크", "지팡이");
+//        // 마크(가) 지팡이(으)로 공격합니다.
+//
+//        a전사.재공격();
+//        // 마크(가) 지팡이(으)로 공격합니다.
+//
+//        a전사.재공격();
 //
 //    }
 //}
-//class 오리 {
-//    void 날다(){
-//        System.out.println("오리가 난다.");
+
+
+
+//class 전사{
+//    String 이름;
+//    String 무기;
+//    void 공격 (String 이름, String 무기){
+//        this.이름=이름;
+//        this.무기=무기;
+//        System.out.printf("%s이(가) %s(으)로 공격합니다.\n",이름,무기);
+//    }
+//    void 재공격(){
+//        System.out.printf("%s이(가) %s(으)로 공격합니다.\n",this.이름,this.무기);
 //    }
 //}
-//
-//
-//class 천둥오리 extends 오리{
-//
-//}
-//class 흰오리 extends 오리{
-//
-//}
-//class 고무오리 extends 오리{
-//    void 날다(){
-//        System.out.println("날수 없음");
-//    }
-//}
-//class 고무2오리 extends 고무오리{
-//
-//}
-
-
-//        최소공배수 두수를 곱한 값의 최대공약수를 나눈값
-//        최대공약수 두수 중 작은 수를 큰수에 나눈 나머지 값
-
-
-//        int numer1=1;
-//        int denom1=2;
-//        int numer2=3;
-//        int denom2=3;
-//
-//        int aa = 0;
-//        int bb =0;
-//
-//        int mm = 0;
-//        int nn = 0;
-//
-//        int[] answer = {};
-//
-//        if(denom1<denom2){
-//            bb=denom2%denom1;
-//
-//        }bb=denom1%denom2;
-//
-//        aa=denom1*denom2/bb;
-//        System.out.println(aa);
-//
-////        answer[0] = 0;
-////        answer[1] = aa;
-//
-//
-//
-//
-////        for(int i=0;i< answer.length;i++) {
-////        }
-//        if(denom1<denom2) {
-//            for (int i = 1; i == aa; i++) {
-//                i * denom1 = aa;
-//                 mm=i;
-//                 System.out.println(i);
-//
-//            }
-//        }
-//        System.out.println(mm);
-//
-//        }
-//    }
-
-
-// 추상메서드만 가지고있는 추상클래스는 인터페이스라고함
-// 추상메서드는 abstract 빼도됨
-// 0
 
 
 
+         int count = 0;
+         int n = 24;
 
+         for(int i=1;i<=n;i++){
+             if(n%i==0){
+                 count++;
+             }
+         }
+        System.out.println(count);
+        int[] answer = new int[]{};
 
-
-
-
-
-
+        int a=0;
+         for(int i=1;i<=n;i++){
+             if(n%i==0){
+                 answer[a]=i;
+                 System.out.println(answer[a]);
+             a++;
+             }
+         }
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-

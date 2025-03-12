35be16587252369bdcec3ecb7a467d39d4ab24cd
qkjm1 <kqk9269@gmail.com>
2025-03-12 15:01:41 +0900

    공부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,8 +40,84 @@ public class Main {
 //        System.out.println("날수 없음");
 //    }
 //}
-//class 고무2오리 extends 오리{
-//    void 날다(){
-//        System.out.println("날수 없음");
-//    }
+//class 고무2오리 extends 고무오리{
+//
 //}
+
+
+//        최소공배수 두수를 곱한 값의 최대공약수를 나눈값
+//        최대공약수 두수 중 작은 수를 큰수에 나눈 나머지 값
+
+
+//        int numer1=1;
+//        int denom1=2;
+//        int numer2=3;
+//        int denom2=3;
+//
+//        int aa = 0;
+//        int bb =0;
+//
+//        int mm = 0;
+//        int nn = 0;
+//
+//        int[] answer = {};
+//
+//        if(denom1<denom2){
+//            bb=denom2%denom1;
+//
+//        }bb=denom1%denom2;
+//
+//        aa=denom1*denom2/bb;
+//        System.out.println(aa);
+//
+////        answer[0] = 0;
+////        answer[1] = aa;
+//
+//
+//
+//
+////        for(int i=0;i< answer.length;i++) {
+////        }
+//        if(denom1<denom2) {
+//            for (int i = 1; i == aa; i++) {
+//                i * denom1 = aa;
+//                 mm=i;
+//                 System.out.println(i);
+//
+//            }
+//        }
+//        System.out.println(mm);
+//
+//        }
+//    }
+
+
+// 추상메서드만 가지고있는 추상클래스는 인터페이스라고함
+// 추상메서드는 abstract 빼도됨
+// 0
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

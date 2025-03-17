dd7a806cb5b4453c830caabf2b0ff9b69ac71c28
qkjm1 <kqk9269@gmail.com>
2025-03-17 17:20:49 +0900

    다시 풀어보기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,26 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Map;
-import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Scanner;;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
+        sc.nextLine();
+        ArrayList[] aa = new ArrayList(a);
 
-        if (b>=45){
-            if (a==0){
-                System.out.println(a+" "+(b-45));
-            }else {
-                System.out.println(a+" "+(b-45));
-            }
+        for (int i = 0; i < a; i++) {
+            String[] c = sc.nextLine().split(" ");
 
-        }else if (b<45){
-            if (a==0){
-                a=24;
-                System.out.println((a-1)+" "+(b+60-45));
-            }else {
-                System.out.println((a-1)+" "+(b+60-45));
-            }
+            aa[i] = Integer.parseInt(c[i]);
+
+
+        }
+
+        for (int i = 0; i < a; i++) {
+            System.out.println(aa[i]);
         }
 
     }

8ea3e8496a3f34c36a6163af3fdd4aa7a7b1dc12
kqjm1 <kqk9269@gmail.com>
2025-03-22 21:39:24 +0900

    객체연결 상속

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,49 +2,101 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+// 문제 : 아래 코드가 실행되도록 해주세요.
+
+class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-        a전사.공격("브라이언", "칼");
-        // 브라이언(이)가 칼(으)로 공격합니다.
+        a샤오미Tv.켜기();
+        // 출력 => 샤오미Tv 켜집니다.
+        a샤오미Tv.끄기();
+        // 출력 => 샤오미Tv 꺼집니다.
+        a샤오미Tv.vr켜기();
+        // 출력 => 샤오미Tv vr켜기!
 
-        a전사.재공격();
-        // 브라이언(이)가 칼(으)로 공격합니다.
+        a삼성Tv.켜기();
+        // 출력 => 삼성Tv 켜집니다.
+        a삼성Tv.끄기();
+        // 출력 => 삼성Tv 꺼집니다.
+        a삼성Tv.ar켜기();
+        // 출력 => 삼성Tv ar켜기!
 
-        a전사.공격("필립", "창");
-        // 필립(이)가 창(으)로 공격합니다.
+        aLGTv.켜기();
+        // 출력 => LGTv 켜집니다.
+        aLGTv.끄기();
+        // 출력 => LGTv 꺼집니다.
+        aLGTv.게임모드전환();
+        // 출력 => LGTv 게임모드전환!
 
-        a전사.공격("마크", "지팡이");
-        // 마크(이)가 지팡이(으)로 공격합니다.
+        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
 
-        a전사.재공격();
-        // 마크(이)가 지팡이(으)로 공격합니다.
+        표준Tv a표준Tv; // 객체선언
 
-        a전사.재공격();
-        // 마크(이)가 지팡이(으)로 공격합니다.
+        a표준Tv = a샤오미Tv;
+        a표준Tv.켜기();
+        // 출력 : 샤오기TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 샤오기TV 꺼집니다.
+
+        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
+        a표준Tv = a삼성Tv;
+        a표준Tv.켜기();
+        // 출력 : 삼성TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 삼성TV 꺼집니다.
+
+        // a표준Tv 변수에 LGTv 객체를 연결한다.
+        a표준Tv = aLGTv;
+        a표준Tv.켜기();
+        // 출력 : LGTV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : LGTV 꺼집니다.
+
+        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
+        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
+        LGTv aLGTv2 = (LGTv)a표준Tv;
+        aLGTv2.게임모드전환();
     }
 }
-class 전사{
-    String 마지막_무기;
-    String 마지막_이름;
 
-    void 공격(String 무기, String 이름){
-        마지막_무기 = 무기;
-        마지막_이름 = 이름;
+class 표준Tv{
+    void 켜기(){}
+    void 끄기(){}
 
-        공격출력(무기, 이름);
-
+}
+class 샤오미Tv extends 표준Tv{
+    void 켜기(){
+        System.out.println("샤오기TV 켜집니다");
+    }
+    void 끄기(){
+        System.out.println("샤오기TV 꺼집니다.");
+    }
+    void vr켜기(){
+        System.out.println("샤오미Tv vr켜기!");
     }
 
-    void 공격출력(String 무기, String 이름){
-        System.out.printf("%s(이)가 %s(으)로 공격합니다.\n",이름,무기);
+}
+class 삼성Tv extends 표준Tv{
+    void 켜기(){
+        System.out.println("삼성Tv 켜집니다");
     }
-
-
-    void 재공격(){
-        System.out.printf("%s(이)가 %s(으)로 공격합니다.\n",마지막_이름,this.마지막_무기);
-
+    void 끄기(){
+        System.out.println("삼성Tv 꺼집니다");
     }
-
+    void ar켜기(){
+        System.out.println("삼성Tv ar켜기!");
+    }
+}
+class LGTv extends 표준Tv{
+    void 켜기(){
+        System.out.println("LGTv 켜집니다.");
+    }
+    void 끄기(){
+        System.out.println("LGTv 꺼집니다.");}
+    void 게임모드전환(){
+        System.out.println("LGTv 게임모드전환!");
+    }
 }

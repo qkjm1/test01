1320208a2013b57d93309dd7af7ed1b228783eca
kqjm1 <kqk9269@gmail.com>
2025-03-22 21:55:34 +0900

    전사 무기변수 상속

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,101 +2,28 @@ package org.example;
 
 import java.util.Scanner;
 
-// 문제 : 아래 코드가 실행되도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
+        전사 a전사 = new 전사();
 
-        a샤오미Tv.켜기();
-        // 출력 => 샤오미Tv 켜집니다.
-        a샤오미Tv.끄기();
-        // 출력 => 샤오미Tv 꺼집니다.
-        a샤오미Tv.vr켜기();
-        // 출력 => 샤오미Tv vr켜기!
-
-        a삼성Tv.켜기();
-        // 출력 => 삼성Tv 켜집니다.
-        a삼성Tv.끄기();
-        // 출력 => 삼성Tv 꺼집니다.
-        a삼성Tv.ar켜기();
-        // 출력 => 삼성Tv ar켜기!
-
-        aLGTv.켜기();
-        // 출력 => LGTv 켜집니다.
-        aLGTv.끄기();
-        // 출력 => LGTv 꺼집니다.
-        aLGTv.게임모드전환();
-        // 출력 => LGTv 게임모드전환!
-
-        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
-
-        표준Tv a표준Tv; // 객체선언
-
-        a표준Tv = a샤오미Tv;
-        a표준Tv.켜기();
-        // 출력 : 샤오기TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 샤오기TV 꺼집니다.
-
-        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
-        a표준Tv = a삼성Tv;
-        a표준Tv.켜기();
-        // 출력 : 삼성TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 삼성TV 꺼집니다.
-
-        // a표준Tv 변수에 LGTv 객체를 연결한다.
-        a표준Tv = aLGTv;
-        a표준Tv.켜기();
-        // 출력 : LGTV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : LGTV 꺼집니다.
-
-        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
-        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
-        LGTv aLGTv2 = (LGTv)a표준Tv;
-        aLGTv2.게임모드전환();
+        a전사.age = 20;
+        a전사.a무기 = new 활(); // .(클래스가 가지고있는 틀(타입 변수))
+        a전사.a무기 = new 칼();
     }
 }
-
-class 표준Tv{
-    void 켜기(){}
-    void 끄기(){}
-
-}
-class 샤오미Tv extends 표준Tv{
-    void 켜기(){
-        System.out.println("샤오기TV 켜집니다");
-    }
-    void 끄기(){
-        System.out.println("샤오기TV 꺼집니다.");
-    }
-    void vr켜기(){
-        System.out.println("샤오미Tv vr켜기!");
-    }
+class 전사{
+    int age;
+    String name;
+    무기 a무기; // class 전사가 가지고있는 무기타입의 이름이 a인 변수
 
 }
-class 삼성Tv extends 표준Tv{
-    void 켜기(){
-        System.out.println("삼성Tv 켜집니다");
-    }
-    void 끄기(){
-        System.out.println("삼성Tv 꺼집니다");
-    }
-    void ar켜기(){
-        System.out.println("삼성Tv ar켜기!");
-    }
+class 무기{
+
 }
-class LGTv extends 표준Tv{
-    void 켜기(){
-        System.out.println("LGTv 켜집니다.");
-    }
-    void 끄기(){
-        System.out.println("LGTv 꺼집니다.");}
-    void 게임모드전환(){
-        System.out.println("LGTv 게임모드전환!");
-    }
+class 활 extends 무기{
+
+}
+class 칼 extends 무기{
+
 }

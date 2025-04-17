fe063a5a7b6997e1326afc448e1ec5bde7601436
qkjm1 <kqk9269@gmail.com>
2025-04-17 13:32:55 +0900

    클래스 연습

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,34 @@ import java.util.Scanner;;
 
 public class Main {
     public static void main(String[] args) {
+        Student student = new Student("물리치료학과", 2019133008);
 
+        System.out.println("학과: "+student.getName()+"\n학번: "+student.getId());
+    }
+}
+
+class Student {
+    private String name;
+    private int id;
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }

d6eb47175feb29bb07613d2733fbb21fa09ea636
qkjm1 <kqk9269@gmail.com>
2025-04-17 16:47:59 +0900

    생성자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(new SalaryMan().getAnnualGross());
+        System.out.println(new SalaryMan(2000000).getAnnualGross());
+    }
+}
+class SalaryMan{
+    private int salary;
+
+    public SalaryMan() {
+        this.salary=1_000_000;
+    }
+
+    public SalaryMan(int salary){
+        this.salary = salary;
+    }
 
 
-        Cylinder cylinder = new Cylinder(18,2);
-        Cylinder cylinder2 = new Cylinder(new Circle(2.8),5.6);
-
-        System.out.println("부피: "+cylinder.getVolume());
-        System.out.println("부피: "+cylinder2.getVolume2());
+    public int getAnnualGross(){
+        return salary*(12+5);
     }
 }
 
